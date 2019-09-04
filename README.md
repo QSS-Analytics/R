39857e26a82cea04c54f348c0c1d2ecafe38866d
Turgut <38523699+henry090@users.noreply.github.com>
2019-09-04 14:55:04 +0400

    R intro

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# R
-Everything related to R
+# R tutorials from QSS Team
+
+This repo will be helpful for newbies / intermediate / advanced users.
+
+The repo includes lots of code chunks for data reading, manipulation, cleaning, building traditional models with ML and etc.
+
+In addition, we provide readers with Deep Learning code chunks via Keras, Tensorflow from R.
+
+The main __purpose__ of this repo is to make everything as simple as possible.
---
+++ Where to start.R
@@ -0,0 +1,44 @@
+
+#-------------------------------------------Step 1-------------------------------------------------------------
+
+# First of all you should download R 
+
+# For windows: https://cran.r-project.org/bin/windows/base/
+# For mac: https://cran.r-project.org/bin/macosx/
+# For linux: https://cran.r-project.org/
+
+# NOTE: For windows you should also download and install Rtools: https://cran.r-project.org/bin/windows/Rtools/
+
+
+#-------------------------------------------Step 2-------------------------------------------------------------
+
+# Then, download and install Rstudio IDE: https://www.rstudio.com/products/rstudio/download/
+
+
+#-------------------------------------------Step 3-------------------------------------------------------------
+
+# We should use Rstudio, not R: https://en.wikipedia.org/wiki/RStudio
+
+# When you open Rstudio, the console should show something as below (e.g R version).
+
+
+# R version 3.6.0 (2019-04-26) -- "Planting of a Tree"
+# Copyright (C) 2019 The R Foundation for Statistical Computing
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+# R is free software and comes with ABSOLUTELY NO WARRANTY.
+# You are welcome to redistribute it under certain conditions.
+# Type 'license()' or 'licence()' for distribution details.
+
+# R is a collaborative project with many contributors.
+# Type 'contributors()' for more information and
+# 'citation()' on how to cite R or R packages in publications.
+
+# Type 'demo()' for some demos, 'help()' for on-line help, or
+# 'help.start()' for an HTML browser interface to help.
+# Type 'q()' to quit R.
+
+
+
+
+
